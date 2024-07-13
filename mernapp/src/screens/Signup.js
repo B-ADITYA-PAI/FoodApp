2380aa22f79fe688487b29840667597089729b43
B-ADITYA-PAI <147653915+B-ADITYA-PAI@users.noreply.github.com>
2024-07-13 16:33:34 +0530

    Update Signup.js

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -8,7 +8,7 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/createuser', {
+    const response = await fetch('https://food-app-woad-iota.vercel.app/createuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
