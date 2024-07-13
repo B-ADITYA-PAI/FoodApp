36229a7d030c2fe4932a63b89d50b1f0a848c7b7
B-ADITYA-PAI <147653915+B-ADITYA-PAI@users.noreply.github.com>
2024-07-13 16:37:49 +0530

    Update Home.js

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [foodItems, setFoodItems] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("https://food-app-woad-iota.vercel.app/foodData", {
+    let response = await fetch("https://food-app-woad-iota.vercel.app/api/foodData", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
