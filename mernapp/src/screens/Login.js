ee79a0f4f587491d747c535522a489b1c584901e
B-ADITYA-PAI <147653915+B-ADITYA-PAI@users.noreply.github.com>
2024-07-13 22:28:47 +0530

    Update Login.js

--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('https://food-app-git-main-b-aditya-pais-projects.vercel.app/api/loginuser', {
+    const response = await fetch('https://localhost:PORT/api/loginuser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
