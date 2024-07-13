7c8e46197f7780701751ac1ece6cb99b0fd651cb
B-ADITYA-PAI <147653915+B-ADITYA-PAI@users.noreply.github.com>
2024-07-13 20:19:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on http://localhost:${port}`);
+// });
