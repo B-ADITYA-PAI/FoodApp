a19f84f72978c0afddd21b3ce8db5b994639a7c0
B-ADITYA-PAI <147653915+B-ADITYA-PAI@users.noreply.github.com>
2024-07-13 16:41:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors({
-  origin: 'https://food-app-woad-iota.vercel.app/'
+  origin: 'https://food-app-woad-iota.vercel.app'
 }));
 
 app.use(express.json());
